1a8dc2487c1132d8a6e5fc30c2ecc23b9d29a153
mohaijiang <mohaijiang110@163.com>
2022-11-23 14:09:51 +0800

    front ci test

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,13 @@ RUN go mod tidy && go build -o a-line-cli
 
 FROM ubuntu:latest
 RUN apt update &&\
-    apt install -y curl git &&\
-    curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+    apt install -y apt-transport-https ca-certificates curl git
+
+RUN curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+RUN chmod a+r /etc/apt/keyrings/docker.gpg
+RUN echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu bionic stable" > /etc/apt/sources.list.d/docker.list
+RUN apt-get update && \
+    apt-get install -y -qq --no-install-recommends docker-ce-cli
 
 COPY --from=builder /app/a-line-cli /usr/local/bin/
 
--- pkg/controller/gin.go
+++ pkg/controller/gin.go
@@ -1,8 +1,14 @@
 package controller
 
 import (
+	"embed"
 	"github.com/gin-gonic/gin"
+	"io/fs"
+	"net/http"
 )
+
+//go:embed dist/*
+var content embed.FS
 
 type HttpServer struct {
 	handlerServer HandlerServer
@@ -16,36 +22,39 @@ func NewHttpService(handlerServer HandlerServer) *HttpServer {
 
 func (h *HttpServer) StartHttpServer() {
 	r := gin.Default()
+	api := r.Group("/api")
 	//create pipeline job
-	r.POST("/pipeline", h.handlerServer.createPipeline)
+	api.POST("/pipeline", h.handlerServer.createPipeline)
 	//update pipeline job
-	r.PUT("/pipeline/:oldName", h.handlerServer.updatePipeline)
+	api.PUT("/pipeline/:oldName", h.handlerServer.updatePipeline)
 	//get pipeline job
-	r.GET("/pipeline/:name", h.handlerServer.getPipeline)
+	api.GET("/pipeline/:name", h.handlerServer.getPipeline)
 	//delete pipeline job and pipeline job detail
-	r.DELETE("/pipeline/:name", h.handlerServer.deletePipeline)
+	api.DELETE("/pipeline/:name", h.handlerServer.deletePipeline)
 	//get pipeline job list
-	r.GET("/pipeline", h.handlerServer.pipelineList)
+	api.GET("/pipeline", h.handlerServer.pipelineList)
 	//get pipeline job detail info
-	r.GET("/pipeline/:name/detail/:id", h.handlerServer.getPipelineDetail)
+	api.GET("/pipeline/:name/detail/:id", h.handlerServer.getPipelineDetail)
 	//delete pipeline job detail
-	r.DELETE("/pipeline/:name/detail/:id", h.handlerServer.deleteJobDetail)
+	api.DELETE("/pipeline/:name/detail/:id", h.handlerServer.deleteJobDetail)
 	//get pipeline job detail list
-	r.GET("/pipeline/:name/details", h.handlerServer.getPipelineDetailList)
+	api.GET("/pipeline/:name/details", h.handlerServer.getPipelineDetailList)
 	//exec pipeline job
-	r.POST("/pipeline/:name/exec", h.handlerServer.execPipeline)
+	api.POST("/pipeline/:name/exec", h.handlerServer.execPipeline)
 	//re exec pipeline detail job
-	r.POST("/pipeline/:name/:id/re-exec", h.handlerServer.reExecuteJob)
+	api.POST("/pipeline/:name/:id/re-exec", h.handlerServer.reExecuteJob)
 	//stop pipeline job
-	r.POST("/pipeline/:name/:id/stop", h.handlerServer.stopJobDetail)
-	r.GET("/pipeline/:name/logs/:id", h.handlerServer.getJobLog)
-	r.GET("/pipeline/:name/logs/:id/:stagename", h.handlerServer.getJobStageLog)
+	api.POST("/pipeline/:name/:id/stop", h.handlerServer.stopJobDetail)
+	api.GET("/pipeline/:name/logs/:id", h.handlerServer.getJobLog)
+	api.GET("/pipeline/:name/logs/:id/:stagename", h.handlerServer.getJobStageLog)
 	// get template list
-	r.GET("/pipeline/templates", h.handlerServer.getTemplates)
-	r.GET("/pipeline/template/:id", h.handlerServer.getTemplateDetail)
-	r.GET("/ping", func(c *gin.Context) {
+	api.GET("/pipeline/templates", h.handlerServer.getTemplates)
+	api.GET("/pipeline/template/:id", h.handlerServer.getTemplateDetail)
+	api.GET("/ping", func(c *gin.Context) {
 		//输出json结果给调用方
 		Success("", c)
 	})
+	fe, _ := fs.Sub(content, "dist")
+	r.NoRoute(gin.WrapH(http.FileServer(http.FS(fe))))
 	r.Run(":8080") // listen and serve on
 }
